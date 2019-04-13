f2e3de8df6aea83708fb67ff13605e7215c93f96
Vlad Jerca <vlad.jerca@gmail.com>
2019-04-14 02:20:06 +0300

    chore: bump version and name package

--- projects/yt-demo/src/app/app.module.ts
+++ projects/yt-demo/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { YtPlayerModule } from '@yt/player';
+import { YtPlayerModule } from '@vladjerca/yt-player';
 
 import { AppComponent } from './app.component';
 
